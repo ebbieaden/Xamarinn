560dd5163cdb34dfdc55de5af5ad407560a409c9
AAAA <AAAA@ADEN>
2022-06-20 04:08:09 -0700

    Add project files.

---
+++ Xamarinn/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarinn.ViewModels;
+
+namespace Xamarinn.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
